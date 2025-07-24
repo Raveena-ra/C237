253ad60869a5c161c89407f7457671cc528c3532
24045392 <157435535+S0LA1NE@users.noreply.github.com>
2025-07-24 12:41:33 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+// In intergrated terminal:
+// npm init -y
+// npm install express
+// npm install ejs
+// npm install mysql2
+// npm install multer
+// To run it:
+// npx nodemon app.js
+const express = require('express');
+const mysql = require('mysql2');
+const multer = require('multer');
+const app = express();
+
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images'); // Directory to save uploaded files
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+
+const connection = mysql.createConnection({
+    host: 'zvu0wa.h.filess.io',
+    user: 'C237PetClinic_smallercap',
+    password: 'password: 6b2fa2706f3bf2aca41d226479935c297597038e',
+    database: 'C237PetClinic_smallercap'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+// Set up view engine
+app.set('view engine', 'ejs');
+
+// enable form processing
+app.use(express.urlencoded({
+    extended: false
+}));
+// enable static files
+app.use(express.static('public'));
